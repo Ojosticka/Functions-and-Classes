9962ba9e0d1da218981b75b91c81aa5218842f3e
Oluwatobi Ojo <oluwatobiojo453@gmail.com>
2025-10-23 23:03:58 +0100

    understanding dunder and magic commands in classes

---
+++ dunder.py
@@ -0,0 +1,68 @@
+# # Dunder/Magic Methods
+
+# class Counter:
+#     def __init__(self):
+#         self.value = 1
+
+#     def count_up(self):
+#         self.value += 1
+#         return self.value
+    
+#     def count_down(self):
+#         self.value -= 1
+
+#     def __str__(self):
+#         return f"Count={self.value}"
+    
+#     def __add__(self, other):
+#         if isinstance(other, Counter):
+#             return self.value + other.value
+#         raise Exception("Invalid type")
+
+
+# count1 = Counter()
+# count2 = Counter()
+
+# count1.count_up()
+# count2.count_up()
+
+# print(count1, count2)
+# print(count1 + 2)
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year 
+
+    # __str__ is meant for a user friendly output
+    def __str__(self):
+        return f"This car is an {self.make} {self.model}, {self.year}"
+
+
+car1 = Car("Hyundai", "L45", 2024)
+
+print(car1)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
